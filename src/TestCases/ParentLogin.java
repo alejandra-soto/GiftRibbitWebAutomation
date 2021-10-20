3e6c85c1cbd71d8919fd95e673a17867ac7ba82d
alejandra-soto <alekins.soto@gmail.com>
2021-10-20 15:36:27 -0700

    TeacherLogin Test Case

--- src/TestCases/ParentLogin.java
+++ src/TestCases/ParentLogin.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-public class TeacherLogin {
+public class ParentLogin {
 
 	public static void main(String[] args) throws InterruptedException {
 
